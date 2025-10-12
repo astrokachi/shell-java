32c5ad9f1bc9ba3093ec61a0d20334bfde1ab895
kachi <astrokachi@gmail.com>
2025-10-12 21:44:31 +0100

    create helper for extracting message and command + type implementation

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,28 +1,46 @@
 import java.util.Scanner;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         // Uncomment this block to pass the first stage
-        while(true) {
+        Set<String> builtin = Set.of("echo", "exit 0", "type");
+
+        while (true) {
             System.out.print("$ ");
             Scanner scanner = new Scanner(System.in);
             String input = scanner.nextLine();
             if (input.equals("exit 0")) {
                 break;
             }
-            
+
             if (input.contains("echo ")) {
-                String[] inputArray = input.split(" ");
-                String[] messageArray = new String[inputArray.length - 1];
-                System.arraycopy(inputArray, 1, messageArray, 0, inputArray.length - 1);
-                System.out.println(String.join(" ", messageArray));
+                String message = extractCmdStrings(input)[1];
+                System.out.println(message);
             }
-            
-            
+
+            if (input.contains("type ")) {
+                String message = extractCmdStrings(input)[1];
+                if (builtin.contains(message)) {
+                    System.out.println(message + " is a shell builtin");
+                } else {
+                    System.out.println(message + ": not found");
+                }
+            }
 
             else {
                 System.out.println(input + ": command not found");
             }
         }
+
+    }
+
+    static String[] extractCmdStrings(String input) {
+        String[] inputArray = input.split(" ");
+        String command = inputArray[0];
+        String[] messageArray = new String[inputArray.length - 1];
+        System.arraycopy(inputArray, 1, messageArray, 0, inputArray.length - 1);
+        String message = String.join(" ", messageArray);
+        return new String[] { command, message };
     }
 }
